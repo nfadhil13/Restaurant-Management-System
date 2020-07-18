e67dae109d1731b291b59fc70dd2e11a96d4436c
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-07-18 10:25:37 +0700

    Nambah Maven

---
+++ src/main/java/controller/BookingController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import model.Booking;
+import org.aspectj.lang.annotation.Aspect;
+import service.repositories.BookingRepo;
+import view.BookingView;
+
+public class BookingController {
+
+    private BookingRepo repo;
+    private BookingView view;
+
+    public BookingController(){
+        repo = BookingRepo.getInstance();
+        view = new BookingView();
+    }
+
+
+    public void addBooking(){
+        Booking newBooking = view.addBooking();
+        int newBookingNumber = 0;
+        for(Booking booking : repo.getAllBooking()){
+            int temp = booking.getBookingNumber();
+            if(temp>newBookingNumber){
+                newBookingNumber = temp;
+            }
+        }
+        newBooking.setBookingNumber(newBookingNumber);
+        repo.insertBooking(newBooking);
+    }
+
+    public void printBookings(){
+        view.printBookings(repo.getAllBooking());
+    }
+
+    public void deleteBooking(){
+        repo.deleteBooking(view.deleteBooking());
+    }
+
+
+
+
+}
